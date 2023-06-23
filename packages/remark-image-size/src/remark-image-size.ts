224e40bf8f7a9061a3447e81fcb19df82a8f0fbb
sor4chi <sorakawa0218@gmail.com>
2023-06-23 09:58:16 +0900

    perf(image-size): optimize image size caching and fetching

--- packages/remark-image-size/src/remark-image-size.ts
+++ packages/remark-image-size/src/remark-image-size.ts
@@ -8,6 +8,8 @@ import type { Image } from 'mdast';
 const fetch = (...args: Parameters<typeof import('node-fetch')['default']>) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+const caches = new Map<string, { width: number; height: number }>();
+
 export const remarkImageSize: Plugin = (): Transformer => {
   return async (tree: Node) => {
     const fetchers: (() => Promise<void>)[] = [];
@@ -15,17 +17,22 @@ export const remarkImageSize: Plugin = (): Transformer => {
       const { url } = node;
       if (url) {
         fetchers.push(async () => {
-          const buffer = await fetch(url).then((res) => res.arrayBuffer());
-          const {
-            metadata: { width, height },
-          } = await getPlaiceholder(Buffer.from(buffer));
+          let metadata = caches.get(url);
+          if (!metadata) {
+            const buffer = await fetch(url).then((res) => res.arrayBuffer());
+            const {
+              metadata: { width, height },
+            } = await getPlaiceholder(Buffer.from(buffer));
+            metadata = { width, height };
+            caches.set(url, { width, height });
+          }
 
           node.data = {
             hProperties: {
               src: url,
               alt: node.alt,
-              width,
-              height,
+              width: metadata.width,
+              height: metadata.height,
             },
           };
         });
